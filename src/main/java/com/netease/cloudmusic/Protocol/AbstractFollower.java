4d82e8bc540db2366e08ef56ce58975321916019
hzzhangmeng2 <476914133@qq.com>
2017-02-06 20:27:37 +0800

    follower接口

---
+++ src/main/java/com/netease/cloudmusic/Protocol/AbstractFollower.java
@@ -0,0 +1,22 @@
+package com.netease.cloudmusic.Protocol;
+
+/**
+ * Created by hzzhangmeng2 on 2017/2/6.
+ */
+
+/**
+ * raft中follower行为定义，follower是基础状态，无论是leader还是candidate，都是从follower转换拓展而来
+ */
+public interface AbstractFollower {
+
+    /**
+     * 1:
+     */
+    void acceptVoteRpc();
+
+    void acceptHeartRpc();
+
+    void acceptAppendRpc();
+
+
+}
